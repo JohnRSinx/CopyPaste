26b78916743357df13e34e7f8b38e1a88a55e179
John Rodrigues <johnrodrigues97106@gmail.com>
2023-08-24 09:43:31 -0300

    correção inputs

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './styles/App.css'
 import React, { useRef, useState } from 'react';
 
 interface CopyInputProps {
+  title: string;
   className: string;
   inputValue: string;
   onInputChange: (value: string, className: string) => void;
 }
 
-const CopyInput: React.FC<CopyInputProps> = ({ className, inputValue, onInputChange }) => {
+const CopyInput: React.FC<CopyInputProps> = ({ className, inputValue, onInputChange ,title }) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleCopyClick = () => {
@@ -25,13 +26,14 @@ const CopyInput: React.FC<CopyInputProps> = ({ className, inputValue, onInputCha
 
   return (
     <div className='input-container'>
-     
+     <p>{title}</p>
       <input
         ref={inputRef}
         className={className}
         type="text"
         value={inputValue}
         onChange={(e) => onInputChange(e.target.value, className)}
+        placeholder={title}
       />
       <button className='buttonCopy' onClick={handleCopyClick}>Copy</button>
     </div>
@@ -67,15 +69,15 @@ function App() {
   return (
     <main className='main'>
       <h1 className='title'>COPY</h1>
-      <CopyInput className='inputcpf' inputValue={inputValues.inputcpf} onInputChange={handleInputChange} />
-      <CopyInput className='data' inputValue={inputValues.data} onInputChange={handleInputChange} />
-      <CopyInput className='nome' inputValue={inputValues.nome} onInputChange={handleInputChange}/>
-      <CopyInput className='outros' inputValue={inputValues.outros} onInputChange={handleInputChange} />
-      <CopyInput className='another' inputValue={inputValues.another} onInputChange={handleInputChange} />
+      <CopyInput className='inputcpf' inputValue={inputValues.inputcpf} onInputChange={handleInputChange} title={'CPF'} />
+      <CopyInput className='data' inputValue={inputValues.data} onInputChange={handleInputChange} title={'DATA'}/>
+      <CopyInput className='nome' inputValue={inputValues.nome} onInputChange={handleInputChange} title={'CEP'}/>
+      <CopyInput className='outros' inputValue={inputValues.outros} onInputChange={handleInputChange} title={'CELULAR'}/>
+      <CopyInput className='another' inputValue={inputValues.another} onInputChange={handleInputChange} title={'E-MAIL'}/>
       <button className='buttonGlobalReset' onClick={handleGlobalReset}>Reset All</button>
       <span>clique no botão "Reset All" para resetar os valores digitados</span>
       <footer>
-        <p>&copy; {new Date().getFullYear()} PH. Todos os direitos reservados.</p>
+        <p className='footer'>&copy; {new Date().getFullYear()} PH. Todos os direitos reservados.</p>
       </footer>
      
     </main>
